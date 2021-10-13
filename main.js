2817beb105146f7e5edaa88feda32a391d947a51
Ефимов Александр <a.efimov@inenergy.ru>
2021-10-13 17:20:06 +0300

    schedule tasks instead of sequential exectuion

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 
-const SEND_TIME = '17:00';
-const NOTIFICATION_TIME = '9:00';
-const CALL_TO_ACTION_TIMEOUT = 10;
-const MODELLING_TIMEOUT = 60;
+const SEND_TIME = '20:00';
+const NOTIFICATION_TIME = '12:00';
+const MODELLING_TIMEOUT = '21:00';
+const UTC_OFFSET = 3;
 const CHANNEL_ID = '894523688106463235';
 
-const notificationMessage = `@everyone Сегодня в ${SEND_TIME} по GMT будет проходить ("НЕЗАЩИЩЕННЫЙ") СПИДМОДЕЛИНГ. Собираемся в дискорд канале Кайно.
+const notificationMessage = `@everyone Сегодня в ${SEND_TIME} по MCK будет проходить СПИДмоделлинг. Собираемся в дискорд канале Кайно.
 
-За ${MODELLING_TIMEOUT} минут вам нужно будет создать модель, по высланному перед стартом рефу.
+За час вам нужно будет создать модель по высланному рефу.
 
 Учавствуйте сами или смотрите за процессом других учатников.`;
 
-const callToActionMessage = `@everyone через ${CALL_TO_ACTION_TIMEOUT} минут спидмоделинг)`;
+// const callToActionMessage = `@everyone через ${CALL_TO_ACTION_TIMEOUT} минут спидмоделинг)`;
 
 const timeoutMessage = `@everyone время вышло, сдаем работы!`;
 
@@ -31,70 +31,31 @@ const client = new Discord.Client({
 
 client.once('ready', async () => {
   console.log('Ready!');
-  mainloop(await fetchMainChannel());
+  scheduleTasks(await fetchMainChannel());
 });
 
 function fetchMainChannel() {
   return client.channels.fetch(CHANNEL_ID);
 }
 
-async function mainloop(channel) {
-  while (1) {
-    try {
-      await notifyUsers(channel);
-      await callUsers(channel);
-      await sendImage(channel);
-      await timeoutCall(channel);
-      currentTopicIndex = (currentTopicIndex + 1) % TOPICS.length;
-    } catch (err) {
-      console.error(err);
-    }
-  }
+function scheduleTasks(channel) {
+  schedule(SEND_TIME, sendImage, channel);
+  schedule(NOTIFICATION_TIME, notifyUsers, channel);
+  schedule(MODELLING_TIMEOUT, timeoutCall, channel);
 }
 
 client.login(process.env.DISCORD_TOKEN);
 
 async function notifyUsers(channel) {
-  await timer(getMillisecondsTimeout(NOTIFICATION_TIME));
   channel.send(notificationMessage);
 }
 
-async function callUsers(channel) {
-  await timer(
-    getMillisecondsTimeout(SEND_TIME) - CALL_TO_ACTION_TIMEOUT * 60 * 1000
-  );
-  channel.send(callToActionMessage);
-}
-
 async function sendImage(channel) {
-  await timer(CALL_TO_ACTION_TIMEOUT * 60 * 1000);
   channel.send(await getImageMessage());
 }
 
 async function timeoutCall(channel) {
-  await timer(MODELLING_TIMEOUT * 60 * 1000);
   channel.send(timeoutMessage);
-}
-
-function timer(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
-
-function getMillisecondsTimeout(desiredTime) {
-  const currentDate = new Date();
-
-  desiredTime = desiredTime.split(':');
-  const desiredDate = new Date();
-  desiredDate.setUTCHours(desiredTime[0]);
-  desiredDate.setUTCMinutes(desiredTime[1]);
-  if (
-    desiredTime[0] < currentDate.getUTCHours() ||
-    (desiredTime[0] == currentDate.getUTCHours() &&
-      desiredTime[1] < currentDate.getUTCMinutes())
-  )
-    desiredDate.setUTCDate(desiredDate.getUTCDate() + 1);
-
-  return desiredDate - currentDate;
 }
 
 async function getImageMessage() {
@@ -132,4 +93,29 @@ async function getImageMessage() {
 function getBackupImage() {
   const backupImages = require('./backup-images.json');
   return backupImages[Math.floor(Math.random() * backupImages.length)];
+}
+
+function schedule(time, task, ...args) {
+  let done;
+  function executeInTime() {
+    if (getCurrentTime() == time) {
+      if (!done) {
+        task(...args)
+          .catch(console.error)
+          .then(() => (done = true));
+      }
+    } else if (done) done = false;
+  }
+  const interval = setInterval(executeInTime, 1000);
+  executeInTime();
+  return function unschedule() {
+    clearInterval(interval);
+  };
+}
+
+function getCurrentTime() {
+  let currentTime = new Date();
+  let currentHours = currentTime.getUTCHours() + UTC_OFFSET;
+  let currentMinuts = currentTime.getUTCMinutes();
+  return `${currentHours}:${currentMinuts}`;
 }
